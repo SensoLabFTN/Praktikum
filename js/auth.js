0513e51f0b320dadd0f476d18b7585095dd84ae3
SensoLabFTN <amslab2018@gmail.com>
2026-02-02 01:58:04 +0100

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,24 @@
+const allowedDomain = "@student.uns.ac.rs"; // PROMENI PO POTREBI
+
+function login() {
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  firebase.auth().signInWithPopup(provider)
+    .then((result) => {
+      const email = result.user.email;
+
+      if (email.endsWith(allowedDomain)) {
+        localStorage.setItem("authorized", "true");
+        localStorage.setItem("userEmail", email);
+        window.location.href = "labs.html";
+      } else {
+        document.getElementById("status").innerText =
+          "Pristup dozvoljen samo za akademske mejlove.";
+        firebase.auth().signOut();
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
 
